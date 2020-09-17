32db21c87a2f75b7cf1ba67bd117b0638052e3fa
Ефимов Александр <a.efimov@inenergy.ru>
2020-09-17 14:01:25 +0300

    fix critical bug

--- preload.js
+++ preload.js
@@ -2,5 +2,5 @@ const { zeroPad } = require('./src/utils/others');
 const { CONFIG_PATH } = require('./src/constants');
 
 window.onload = () => {
-  document.getElementById('id').innerText = zeroPad(require(CONFIG_PATH).id, 3);
+  document.getElementById('id').innerText = zeroPad(require(CONFIG_PATH + '/settings.json').id, 3);
 };
