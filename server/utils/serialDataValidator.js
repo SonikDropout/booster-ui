4a428938c283c96e0b7f1cab539b8dcb33988ebe
Ефимов Александр <a.efimov@inenergy.ru>
2021-03-19 19:39:27 +0300

    fix serial data validator

--- server/utils/serialDataValidator.js
+++ server/utils/serialDataValidator.js
@@ -10,17 +10,17 @@ module.exports = function validate(buffer) {
     throw new Error('Invalid buffer recieved');
   let i = 0;
   let checkSum = 0;
-  while (i < SEPARATORS.length + PARAMS_DATA.length) {
-    checkSum += buf.readInt16BE(i);
+  while (i < SEPARATORS.length + PARAMS_DATA.length * 2) {
+    checkSum += buffer.readInt16BE(i);
     i += 2;
   }
   for (let j = 0; j < STATE_DATA.length; j++) {
-    checkSum += buf[i++];
+    checkSum += buffer[i++];
   }
   checkSum = checkSum % Math.pow(2, 16);
-  if (checkSum != buf.readUInt16BE(i)) {
+  if (checkSum != buffer.readUInt16BE(i)) {
     throw new Error(
-      `Check sums don't match calculated: ${checkSum}, recieved: ${buf.readUInt16BE(
+      `Check sums don't match calculated: ${checkSum}, recieved: ${buffer.readUInt16BE(
         i
       )}`
     );
