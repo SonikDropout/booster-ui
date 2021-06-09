31cf3e8e3ecea8dcd23ad01573ecba2e47ccff75
Ефимов Александр <a.efimov@inenergy.ru>
2021-06-09 17:16:02 +0300

    remove unused parameters from ui

--- client/models/paramsLayout.js
+++ client/models/paramsLayout.js
@@ -36,11 +36,7 @@ module.exports = [
       values: [
         { name: 'temp1', maxCompare: 'maxTemp' },
         { name: 'temp2', maxCompare: 'maxTemp' },
-        { name: 'temp3', maxCompare: 'maxTemp' },
-        { name: 'temp4', maxCompare: 'maxTemp' },
-        { name: 'temp5', maxCompare: 'maxTemp' },
         { name: 'radiatorTemp1' },
-        { name: 'radiatorTemp2' },
       ],
     },
   ],
